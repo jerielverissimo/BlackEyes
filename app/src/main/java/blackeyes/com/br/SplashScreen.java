f5f109aceba2217d13ded53d78e00be8473181ac
jeriel <jeriel.verissimo@outlook.com>
2015-10-19 20:32:28 -0200

    Splash screen

--- app/src/main/java/blackeyes/com/br/SplashScreen.java
+++ app/src/main/java/blackeyes/com/br/SplashScreen.java
@@ -1,13 +1,30 @@
 package blackeyes.com.br;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+
+
+
+public class SplashScreen extends AppCompatActivity implements Runnable{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Handler SplashScreen = new Handler();
+        SplashScreen.postDelayed(SplashScreen.this, 3000);
+
     }
+
+    @Override
+    public void run() {
+        startActivity(new Intent(SplashScreen.this, MainActivity.class));
+        finish();
+    }
+
+
 }
